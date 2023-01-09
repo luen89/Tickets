f408bae02e9360d0f18b9a0d7f0e0269b779335d
Luis Enrique Pérez González <luen89@live.com.mx>
2023-01-08 21:41:54 -0600

    Co-authored-by: JulioPeloz <JulioPeloz@users.noreply.github.com>

---
+++ tickets/src/Header.js
@@ -0,0 +1,12 @@
+
+export const Header = () => {
+    return (
+        <>
+            <head>
+                <div>
+                    <input type="date"></input>
+                </div>
+            </head>
+        </>
+    );
+}
